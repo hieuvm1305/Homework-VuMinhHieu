fd6b047cc7070216764a5379c2d4cd537d6705d0
hieuvm1305 <hieuvm1305@gmail.com>
2022-09-16 13:30:05 +0700

    new

--- Promise/Promise.js
+++ Promise/Promise.js
@@ -1,29 +1,32 @@
-//cau 1
+const { func } = require("prop-types");
+
 class Queue {
   constructor(delay = 1000) {
     this.delay = delay;
-    this.queue = [];
+    this.callbackQueue = [];
   }
 
   add(callback) {
-    //queue
-    if (this.queue.length == 0) {
-      //callback sẽ dc chạy luôn
-      this.queue.push(callback);
-      callback();
+    if (this.callbackQueue.length === 0) {
+      this.callbackQueue.push(callback);
+      this.runcallback();
     } else {
-      this.queue.push(callback);
-      setTimeout(callback, this.delay); //setTimeout(callback, this.delay * this.queue.indexOf(callback));
+      this.callbackQueue.push(callback);
     }
   }
+
+  runcallback() {
+    const callback = this.callbackQueue[0];
+    setTimeout(() => {
+      this.callbackQueue.shift();
+      if (this.callbackQueue.length > 0) {
+        this.runcallback();
+      }
+    }, this.delay);
+
+    callback();
+  }
 }
-let test = new Queue();
-test.add(() => console.log("hello"));
-//
-test.add(() => console.log("hello 2"));
-//
-test.add(() => console.log("hello 3"));
-// nếu add từng callback thì dùng this.delay, nếu dùng nhiều lệnh add cùng một lúc thì dùng timeout = this.delay * this.queue.indexof(callback).
 
 //cau 2
 const api = "https://api.github.com/search/repositories?q=";
@@ -41,61 +44,135 @@ const array = [
   "css",
   "pug",
 ];
-
-const getApi = (array) => {
-  let arrayTemp = [...array];
-  let keyError = [];
-  //get 5 element first
-  while (arrayTemp.length) {
-    let keyArray = arrayTemp.slice(0, 5);
-    let apis = keyArray.map((keyword) => fetch(api + keyword));
-    Promise.allSettled(apis).then((response) => {
-      console.log(response[0]);
-      response.forEach((res) => {
-        if (res.value.status != 200) {
-          let url = res.value.url;
-          let keyErr = url.slice(url.indexOf("=") + 1);
-          keyError.push(keyErr);
-          //.... Wait for magic
+const promiseApi = () => {
+  const keyWord = array.splice(0, 5);
+  if (keyWord.length < 1) {
+    return;
+  }
+  const promiseApi = keyWord.map((item) => fetch(api + item)); //clear
+  let rejectKeyword = [];
+  Promise.allSettled(promiseApi)
+    .then((response) => {
+      let fullfilled = [];
+      response.forEach((res, index) => {
+        if (res.status === "fulfilled") {
+          fullfilled.push(res.value.json());
+        } else {
+          rejectKeyword.push(keyWord[index]);
         }
       });
-    });
-    let arrFilter = arrayTemp.filter((key) => !keyArray.includes(key)); //Loại bỏ các key đã gọi api
-    arrayTemp = arrFilter; //gán mảng để chạy tiếp
-  }
+      return Promise.all(fullfilled);
+    })
+    .then(() => promiseApi());
 };
-getApi(array);
+promiseApi();
+
+const keywords = [
+  "swift",
+  "rust",
+  "javascript",
+  "react",
+  "rails",
+  "ruby",
+  "php",
+  "rx",
+];
+const logs = [];
+const requestOptions = {
+  method: "GET",
+  redirect: "follow",
+};
+
+async function runSearch() {
+  if (keywords.length > 0) {
+    const currentSearchKeywords = keywords.splice(0, 5); //Cắt 5 phần tử đầu tiên ra khỏi mảng key
+
+    const promises = currentSearchKeywords.map((keyword) => {
+      return fetch(
+        `https://api.github.com/search/repositories?q=${keyword}`,
+        requestOptions
+      );
+    }); //Phần tạo request
+
+    const searchResponses = await Promise.allSettled(promises); //gọi 5 api
+
+    const jsonDataList = Promise.all(
+      searchResponses.map((response) => response.value.json())
+    );
+    //mảng trả kết quả về
+
+    for (let i = 0; i < jsonDataList.length; i++) {
+      const jsonData = jsonDataList[i];
+
+      if (
+        searchResponses[i].status === "fulfilled" &&
+        searchResponses[i].value.status === 200 //Kiem tra xem có success hay không
+      ) {
+        console.log(`keywords: ${currentSearchKeywords[i]} - success: ${true}`);
+        console.log("first item: ", jsonData.items[0]);
+      } else {
+        console.log(
+          `keywords: ${currentSearchKeywords[i]} - success: ${false}`
+        );
+        console.log("first item: ", "404 not found");
+        keywords.push(currentSearchKeywords[i]);
+      }
+    }
+    return runSearch(); //De quy
+  } else return;
+}
+runSearch();
 
 //Câu 3
 const randomPromise = new Promise((resolve, reject) => {
-  let count = 0;
   for (let i = 0; i < 5; i++) {
     let res = parseInt(Math.random() * 100);
-    if(res % 2 == 0){
+    if (res % 2 == 0) {
       resolve(res);
-      count = 1;
-      break;
     }
   }
-  if(count == 0){
-    reject('Not Find');
-  }
+  reject("Not Find");
 });
-randomPromise.then(value => console.log(value), error => console.log(error))
+randomPromise
+  .then((value) => console.log(value))
+  .catch((error) => console.log(error));
 
- /*
- const ar = [1, 3, 7, 5, 9]
-const randomPromise = new Promise((resolve, reject) => {
-  let count = 0;
-  for (let i = 0; i < 5; i++) {
-    if(ar[i] % 2 == 0){
-      resolve(res);
-      count = 1;
-      break;
-    }
-  }
-  if(count == 0){
-    reject('Not Find');
-  }
-});
-randomPromise.then(value => console.log(value), error => console.log(error))*/
+//Debounce: đảm bảo một hàm không được thực thi cho đến một khoảng thời gian nhất định sau khi event không dc thực hiện
+
+const debounce1 = (callback, timeout) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer); // bỏ timeout cho timer.
+    timer = setTimeout(() => callback.apply(this, args), timeout); //apply(tham khảo): gọi vào giá trị của this và các đối số truyền vào dưới dạng mảng.this
+  };
+};
+const test = debounce1(() => console.log("hello"), 1000);
+test();
+
+//Throttle: Mỗi khoảng thời gian mới gọi hàm
+let wait = false;
+const throttle = (callback, timeout) => {
+    if (wait) return;
+    wait = true;
+    setTimeout(() => {
+      callback();
+      wait = false;
+    }, timeout);
+}
+const test1 = throttle(() => console.log("object"), 500)
+console.log(test1)
+
+//Stole from Internet
+function throttle(func, delay) {
+  let lastCall = 0;
+
+  return function (...args) {
+      const now = new Date().getTime();
+
+      if (now - lastCall < delay) {
+          return;
+      }
+      lastCall = now;
+      return func(...args);
+  };
+}
